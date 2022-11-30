f82b17b508850a7e34fbbc9fb0bd71c177f70a8e
adityaSTL <111113768+adityaSTL@users.noreply.github.com>
2022-11-30 11:33:04 +0530

    added get_ms
    It includes milestones in the image

--- main3.py
+++ main3.py
@@ -5,8 +5,6 @@ import matplotlib.image as mpimg
 import numpy as np
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
-global y_divider1,y_divider2,y_road1,y_road2,y_roadside1,y_roadside2,x,len,y
-y,x,len=0,2000,1000
 
 def annonater(y1,y2,xl=2500,text=""):
         plt.annotate(
@@ -32,8 +30,6 @@ def get_divider(d=300):
     plt.hlines(y_divider2,x,x+len,linestyles='solid',colors='black',lw=1)
     annonater(y_divider1,y_divider2,x+400)
 
-
-
 def get_road(d1=900,d2=900,d3=850):
     global y_road1,y_road2
     y_road1=y_divider1+d1
@@ -52,8 +48,6 @@ def get_road(d1=900,d2=900,d3=850):
     ##Get ofc line
     plt.hlines(y_divider2-d2/2-d3,x,x+len,linestyles='dashed',colors='red',lw=2)
     annonater(y_divider2-d2/2,y_divider2-d2/2-d3,x+400)
-
-
 
 def get_roadside(d1=500,d2=500):
     global y_roadside1,y_roadside2
@@ -75,6 +69,7 @@ def get_objects_roadside(d1=500,d2=700):
     yl=y_roadside1+d1
     yr=y_roadside2-d1   
     df=get_df()
+    ax = plt.gca() 
     #print(df.index)
     #print(df,len(df.index))
     for i in df.index:
@@ -124,8 +119,6 @@ def get_objects_roadside(d1=500,d2=700):
             ab1 = AnnotationBbox(imagebox1, (xx, yy), frameon = False)
             ax.add_artist(ab1)
         
-
-
 def get_chainage(d=1400):
     df=get_df()
     yr=y_roadside2-d
@@ -135,6 +128,7 @@ def get_chainage(d=1400):
 
 def get_culvert():
     df=get_df()
+    ax = plt.gca() 
     for i in df.index:
         if df['Culvert'][i]=="Yes":
             tx1, ty1 = df['Chainage'][i]+20,y_roadside1
@@ -157,6 +151,7 @@ def get_culvert():
             
 def get_bridge():
     df=get_df()
+    ax = plt.gca() 
     for i in df.index:
         if df['Bridge Name'][i]=="Yes":
             tx1, ty1 = df['Chainage'][i]+20,y_roadside1
@@ -179,6 +174,7 @@ def get_bridge():
 
 def get_serviceroad():
     df=get_df()
+    ax = plt.gca() 
     for i in df.index:
         if df['Service Road Name'][i]=="Yes":
             tx1, ty1 = df['Chainage'][i]+20,y_road1
@@ -223,59 +219,76 @@ def get_fixed_things(d=0):
     plt.text(2900,-1300,"Gravel",fontsize=9,color='Green')
     plt.text(2500,-1300,"Gravel",fontsize=9,color='Green')          
 
-
-
 def get_soil(d=1600):
     df=get_df()
     for i in df.index:
         plt.text(float(df['Chainage'][i]),y_roadside2-d,df['Soil Types'][i],fontsize = 5,color='Black')
 
+
+def get_ms():
+    df=get_df()
+    ax = plt.gca() 
+    path=r"MS_box.png"
+    img = mpimg.imread(path)
+    imagebox1 = OffsetImage(img, zoom = 0.15)
+    i=df['Chainage'].iloc[0]
+    f=df['Chainage'].iloc[-1]
+    plt.text(i-50,y_road1,"MS-"+str(round(i/1000)),fontsize = 9,color='Black')
+    plt.text(f+100,y_road1,"MS-"+str(round(f/1000)),fontsize = 9,color='Black')
+    ab1 = AnnotationBbox(imagebox1,(i-35,y_road1+300), frameon = False)
+    a1 = AnnotationBbox(imagebox1,(f+115,y_road1+300), frameon = False)
+    ax.add_artist(ab1)  
+    ax.add_artist(a1)
+
 def draw_sld():
-    try:
-        get_road(lwidth,rwidth)  #Drawing roads (left +Right)
-        get_roadside(lwidth,rwidth) #Drawing roadside lines (left + Right)
-        get_divider(d_half) #Drawing divider on road (at half of divider width)
-        get_objects_roadside() #Drawing road side objects (if images)
-        get_ofc_line(d)
-        get_culvert()
-        #get_service_road(width,side,name)
-        #get_fixed_things(coords)
-        get_bridge(desc,coords,name)
-        get_soil()
-        get_chainage()
+    global y_divider1,y_divider2,y_road1,y_road2,y_roadside1,y_roadside2,x,len,y
+    y,x,len=0,2000,1000
+    fig=plt.figure(figsize=(200, 50))  
+    fig1 = plt.gcf()
+    ax = plt.gca() 
+    get_divider()
+    get_road()
+    get_roadside()
+    outer_box()
+    get_objects_roadside()
+    get_chainage()
+    get_culvert()
+    get_bridge()
+    get_soil()
+    get_serviceroad()
+    get_fixed_things()
+    coords()
+    get_ms()
 
-    except:
-        print("All functions still not active")
-
-
-
-
-
-fig=plt.figure(figsize=(200, 50))   
-ax = plt.gca()
-#ax.set_xlim(x-90, x+len+90)
-plt.axis('off')
-get_divider()
-get_road()
-get_roadside()
-outer_box()
-get_objects_roadside()
-get_chainage()
-get_culvert()
-get_bridge()
-get_soil()
-get_serviceroad()
-get_fixed_things()
-coords()
-fig1 = plt.gcf()
-plt.show()
-plt.draw()
-fig1.savefig('tesssttyyy.png', dpi=300)
+    #ax.set_xlim(x-90, x+len+90)
+    plt.axis('off')
+    fig1 = plt.gcf()
+    plt.show()
 
 
 
 
+draw_sld()
+# fig=plt.figure(figsize=(200, 50))  
+# fig1 = plt.gcf()
+# ax = plt.gca() 
+# get_divider()
+# get_road()
+# get_roadside()
+# outer_box()
+# get_objects_roadside()
+# get_chainage()
+# get_culvert()
+# get_bridge()
+# get_soil()
+# get_serviceroad()
+# get_fixed_things()
+# coords()
 
+# #ax.set_xlim(x-90, x+len+90)
+# plt.axis('off')
+# fig1 = plt.gcf()
+# plt.show()
 
 
 
